27345bdaadd4fe2a50eb0b1f3e23edd5043d81ab
Micromolecule001 <dima.boyko.bd@gmail.com>
2024-06-20 16:22:36 +0300

    meged binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -6,7 +6,7 @@ const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 let BinarySearch = (arr, e, start, end) => {
     itterationCount++
-    let middle = Math.floor( (start + end)/2 )
+    let middle = Math.floor((start + end) / 2)
     if (e === arr[middle]) {
         return middle
     } else if (e < arr[middle]) {
